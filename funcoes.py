10a8dbad9d47223bd132dd923c0e39c024a83f3a
Feiticeiro Tecnológico <53744463+feiticeiro-tec@users.noreply.github.com>
2022-04-12 01:24:50 -0300

    Trocando open e close por with

--- funcoes.py
+++ funcoes.py
@@ -1,16 +1,5 @@
 def ler_arquivo():
 
     # Lê arquivo de palavras.txt
-    file = open("palavras.txt", "r")
-
-    lista_palavras = []
-
-    # Formata e joga as palavras para uma lista python
-    for palavra in file:
-        palavra = palavra.strip()
-        palavra = palavra.casefold()
-        lista_palavras.append(palavra)
-
-    file.close()
-
-    return lista_palavras
+    with open('palavras.txt','r') as file:
+    	return list(palavra.strip().casefold() for palavra in file)
